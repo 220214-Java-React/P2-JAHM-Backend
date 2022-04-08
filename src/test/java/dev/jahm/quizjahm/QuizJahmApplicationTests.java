8c690db028851bfb25e130dfb9a83b5a2bf391b9
Asheton S. M <AshetonSM@gmail.com>
2022-04-08 10:17:32 -0500

    Initial spring boot configuration.

---
+++ src/test/java/dev/jahm/quizjahm/QuizJahmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jahm.quizjahm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizJahmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
