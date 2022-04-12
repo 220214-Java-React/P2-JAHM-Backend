3067c6f84ff187eef42a92ea2f54ec2d296b1e5f
Asheton S. M <AshetonSM@gmail.com>
2022-04-12 16:44:01 -0500

    Added a test to check the response of an endpoint.

--- src/test/java/dev/jahm/quizjahm/UserServiceTest.java
+++ src/test/java/dev/jahm/quizjahm/UserServiceTest.java
@@ -4,17 +4,32 @@ import dev.jahm.quizjahm.controller.UserController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
 public class UserServiceTest {
+
+    @LocalServerPort
+    private int port;
 
     @Autowired
     UserController userController;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     void contextLoads() throws Exception {
         assertThat(userController).isNotNull();
+    }
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/users/hello",
+                String.class)).contains("Hello, World");
     }
 }
