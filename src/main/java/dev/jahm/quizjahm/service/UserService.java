d60898eaca98bfa15af20a7a9b79cda4aea143bf
Asheton S. M <AshetonSM@gmail.com>
2022-04-11 22:59:15 -0500

    UserService skeleton with getAllUsers and createNewUser methods.

--- src/main/java/dev/jahm/quizjahm/service/UserService.java
+++ src/main/java/dev/jahm/quizjahm/service/UserService.java
@@ -1,4 +1,23 @@
 package dev.jahm.quizjahm.service;
 
+import dev.jahm.quizjahm.model.User;
+import dev.jahm.quizjahm.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void createNewUser(User user){
+        userRepository.save(user);
+    }
 }
