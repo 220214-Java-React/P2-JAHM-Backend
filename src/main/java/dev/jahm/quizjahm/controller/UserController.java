19a705881e098a0e8756bff8a4036c0e8172c2d5
Asheton S. M <AshetonSM@gmail.com>
2022-04-11 22:57:20 -0500

    Basic POST/GET user skeleton.

--- src/main/java/dev/jahm/quizjahm/controller/UserController.java
+++ src/main/java/dev/jahm/quizjahm/controller/UserController.java
@@ -1,4 +1,29 @@
 package dev.jahm.quizjahm.controller;
 
+import dev.jahm.quizjahm.model.User;
+import dev.jahm.quizjahm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    @CrossOrigin
+    public void createNewUser(@RequestBody User user){
+        userService.createNewUser(user);
+    }
+
+    @GetMapping
+    @CrossOrigin
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
 }
