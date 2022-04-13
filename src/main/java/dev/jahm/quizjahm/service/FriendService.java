16eaf87ae3f0f7dff74d4e75fd80298c9dfacf6a
Asheton S. M <AshetonSM@gmail.com>
2022-04-13 15:12:50 -0500

    Added getFriendsByUser and some documentation.

--- src/main/java/dev/jahm/quizjahm/service/FriendService.java
+++ src/main/java/dev/jahm/quizjahm/service/FriendService.java
@@ -1,6 +1,7 @@
 package dev.jahm.quizjahm.service;
 
 import dev.jahm.quizjahm.model.Friend;
+import dev.jahm.quizjahm.model.User;
 import dev.jahm.quizjahm.repository.FriendRepository;
 import dev.jahm.quizjahm.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,14 @@ public class FriendService {
 
     public List<Friend> getAllFriends() {
         return friendRepository.findAll();
+    }
+
+    /**
+     * Returns all Friend entries where the supplied User is either user1 or user2.
+     * @param user
+     * @return
+     */
+    public List<Friend> getFriendsByUser(User user) {
+        return friendRepository.findByUser1OrUser2(user, user);
     }
 }
