a76caf72589ff165230cf4db346e870cebc63ee3
Asheton S. M <AshetonSM@gmail.com>
2022-04-13 15:10:30 -0500

    Added by ID get mapping. Added all mapping for debugging.

--- src/main/java/dev/jahm/quizjahm/controller/FriendController.java
+++ src/main/java/dev/jahm/quizjahm/controller/FriendController.java
@@ -2,7 +2,9 @@ package dev.jahm.quizjahm.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.jahm.quizjahm.model.Friend;
+import dev.jahm.quizjahm.model.User;
 import dev.jahm.quizjahm.service.FriendService;
+import dev.jahm.quizjahm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,10 +17,18 @@ public class FriendController {
     @Autowired
     FriendService friendService;
 
-    @GetMapping
+    @Autowired
+    UserService userService;
+
+    /**
+     * Returns the friends list of the user matching this ID number
+     * @param id User ID to search for
+     * @return Friend list
+     */
+    @GetMapping("/{id}")
     @CrossOrigin
-    public List<Friend> getAllFriends() {
-        return friendService.getAllFriends();
+    public List<Friend> getFriendsByUser(@PathVariable Integer id) {
+        return friendService.getFriendsByUser(userService.getUserById(id));
     }
 
     /**
@@ -31,6 +41,16 @@ public class FriendController {
         Integer friend1 = objectNode.get("friend1").asInt();
         Integer friend2 = objectNode.get("friend2").asInt();
         friendService.createNewFriend(friend1, friend2);
+    }
+
+    /**
+     * Mainly for debug purposes. Returns all Friend objects in the system.
+     * @return
+     */
+    @GetMapping("/all")
+    @CrossOrigin
+    public List<Friend> getAllFriends() {
+        return friendService.getAllFriends();
     }
 
 }
