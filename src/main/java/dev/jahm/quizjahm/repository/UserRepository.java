e75b53ad3203c8023e4819a3905097ba4aaaf2d7
Asheton S. M <AshetonSM@gmail.com>
2022-04-14 15:50:24 -0500

    Added findByUsername.

--- src/main/java/dev/jahm/quizjahm/repository/UserRepository.java
+++ src/main/java/dev/jahm/quizjahm/repository/UserRepository.java
@@ -1,8 +1,19 @@
 package dev.jahm.quizjahm.repository;
 
+import dev.jahm.quizjahm.model.Friend;
 import dev.jahm.quizjahm.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> { }
+public interface UserRepository extends JpaRepository<User, Integer> {
+    /**
+     * Return a Friend list of Users who are listed as user1 or user2.
+     * Both arguments (user1 and user2) are meant to be the same when calling this method.
+     * @param username Username to find
+     * @return Matching User
+     */
+    User findByUsername(String username);
+}
